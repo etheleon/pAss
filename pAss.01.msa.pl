57d6417858d96b58b6cdbeeb324e070fee0ffbca
etheleon <picy2k@gmail.com>
2015-03-23 13:36:30 +0800

    checks MEGAN's the STDOUT if dun have try again

--- pAss.01.msa.pl
+++ pAss.01.msa.pl
@@ -1,9 +1,10 @@
 #!/usr/bin/env perl
 
-use Modern::Perl '2014';
+use Modern::Perl '2015';
 use autodie;
+use experimental qw/signatures/;
 
-die "usage: $0 query_file blast_file output.dir MEGAN\n" unless $#ARGV == 2;
+die "usage: $0 query_file blast_file output.dir MEGAN\n" unless $#ARGV == 3;
 
 my ($query, $blast, $out, $megan) = @ARGV;
 
@@ -12,42 +13,40 @@ die unless -f $blast;
 die unless -f $megan;
 
 my $temp = rand().time();
-my $CMD = join '', <DATA>;
-$CMD =~ s/blastx.txt/$blast/;
-$CMD =~ s/blastx.rma/$temp.rma/;
-$CMD =~ s/query.fna/$query/;
-$CMD =~ s/example/$out/;
-open CMD, ">$temp" or die;
-print CMD $CMD;
-close CMD;
-mkdir $out unless -d $out;
 
+prep($blast, $temp, $query, $out, $megan);
+runMegan($src, $out, $temp, $stdFile, $megan, 1);
 
-my $i;
-my $signal = -1;
-while($signal != 0)
-{
-    $i++;
-    my $scr = int(30000 * rand());
-    while(-e "/tmp/.X$scr-lock")
-    {
-        $scr = int(30000 * rand());
-    }
+sub runMegan ($src, $out, $temp, $megan, $count){
+    $count < 20 ? $count++ : return;
+
     unlink "$out.lock" if -e "$out.lock";
     unlink "$out.log" if -e "$out.log";
-    unlink "$out.err" if -e "$out.err";
-    $signal = system "xvfb-run -n $scr -f $out.lock -e $out.log ~/local/megan/MEGAN -g -d -E  -c $temp";
-    unless($signal == 0)
-    {
-        system("mv $out.log $out.err");
-        system("echo $signal >> $out.err");
-    }
-    unlink "$out.lock";
-    last if $i > 20;
-}
-unlink $temp;
-unlink "$temp.rma";
 
+    my $scr = int(30000 * rand());
+
+    #try new screen number if its taken;
+    $scr = int(30000 * rand()) while -e "/tmp/.X$scr-lock";
+
+    my $signal = `xvfb-run -n $scr -f $out.lock -e $out.log $megan -g -d -E  -c $temp`;
+
+    #cant check for xvfb-run's own error [xc's version]
+    $signal =~ m/Writing/sm ? eval{unlink "$out.lock", $temp, "$temp.rma": return} : runMegan($src, $out, $temp, $stdFile, $megan, $count);
+}
+
+sub prep ($blast, $temp, $query, $out, $megan){
+    my $CMD = join '', <DATA>;
+    $CMD =~ s/blastx.txt/$blast/;   #text file
+    $CMD =~ s/blastx.rma/$temp.rma/;
+    $CMD =~ s/query.fna/$query/;
+    $CMD =~ s/example/$out/;
+
+    open my $cmd, ">$temp" or die;
+    print $cmd $CMD;
+    close my $cmd;
+
+    mkdir $out unless -d $out;
+}
 
 __END__
 import blastFile='blastx.txt' fastaFile='query.fna' meganFile='blastx.rma' maxMatches=25 minScore=50.0 maxExpected=1.0 topPercent=100.0 minSupport=1 minComplexity=0.30 useSeed=false useCOG=false useKegg=false paired=false useIdentityFilter=false textStoragePolicy=1 blastFormat=BlastX mapping='Taxonomy:BUILT_IN=true';
