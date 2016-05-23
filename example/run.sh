c5e60ae6584752972fa507f5db7ffc4c92de6eac
etheleon <picy2k@gmail.com>
2016-05-23 12:11:55 +0800

    Update run.sh

--- example/run.sh
+++ example/run.sh
@@ -4,10 +4,10 @@
 #Run this inside the /pAss/example folder
 
 #clear up the rubbish files
-#rm -rf testData
-#rm -rf testOut
-#cp -r $PWD/data $PWD/testData
-#cp -r $PWD/out $PWD/testOut
+rm -rf testData
+rm -rf testOut
+cp -r $PWD/data $PWD/testData
+cp -r $PWD/out $PWD/testOut
 
 #download MEGAN (need to test)
 #wget http://ab.inf.uni-tuebingen.de/data/software/megan5/download/MEGAN_unix_5_11_3.sh
@@ -19,5 +19,5 @@
     --outputDIR=$PWD/testOut \
     --refseqKO=$PWD/testData/refSeqProtDB \
     --contigs=$PWD/testData/contigs \
-    --megan=$PWD/local/bin/MEGAN \
+    --megan=/usr/local/bin/MEGAN \
     --meganLicense=$PWD/MEGAN5-academic-license.txt
